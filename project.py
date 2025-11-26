2f83887da434fec899956d9f6155378a50df166f
kodzip1515 <kodzip1515@gmail.com>
2025-11-26 14:35:08 +0000

    made a  password checker

--- project.py
+++ project.py
@@ -1,1 +1,14 @@
-print("hello world")
+#init
+password = "hello"
+guess = 0
+passguess = ""
+
+#process
+for x in range(3):
+    passguess = input("please enter your password: ")
+    if passguess == password:
+        print("correct password")
+        break
+    guess = guess + 1
+    if guess == 3:
+        print("error to many attemps")
